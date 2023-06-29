c5ccdbe02da826599230cff6e80fdbcb9e26202b
vivek.deshmukh <vivek.x.deshmukh.-nd@disney.com>
2023-06-29 11:42:43 +0530

    Updated java repo

---
+++ src/main/java/com/visualpathit/account/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.visualpathit.account.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.visualpathit.account.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findById(long id);
+    
+}
